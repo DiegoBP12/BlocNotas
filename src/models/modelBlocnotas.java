cdb332d44a8677beb276565377c4fdd4599925d2
Diego Bolaños Pardo <37430615+DiegoBP12@users.noreply.github.com>
2018-09-18 19:29:19 -0500

    Se generan las variables a utilizar para los métodos plasmados en el control

--- src/models/modelBlocnotas.java
+++ src/models/modelBlocnotas.java
@@ -4,11 +4,46 @@
  * and open the template in the editor.
  */
 package models;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 /**
- *
+ * Se generan las variables a utilizar 
+ * Path: para la ruta que va a recibir la ruta del archivo.
+ * Mensaje: es el el texto que se guardara el archivo.
+ * row: es la fila que existe en el mensaje.
  * @author Diego
  */
 public class modelBlocnotas {
-    
+       private String path;
+       private String mensaje;
+       private String row;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public void setRow(String row) {
+        this.row = row;
+    }
 }
